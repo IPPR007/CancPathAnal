4b79e89b1732248edcd188bf907d5dc5fd7ed868
IPPR007 <fzhan@foxmail.com>
2024-11-16 14:49:00 +0800

    delete model_validation

--- model_validation.R
+++
@@ -1,9 +0,0 @@
-library(riskRegression)  
-library(pec)  
-library(survival)  
-
-source("functions/load_data.R")  
-
-source("functions/calibration_plots.R")  
-
-source("functions/roc_analysis.R")  
